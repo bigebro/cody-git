37cc8e691248ad5aa39d8e781c4b383616474064
bilguun.e <bigeservice@gmail.com>
2022-01-04 20:34:49 +0800

    deploy

--- src/_desktop/Home.js
+++ src/_desktop/Home.js
@@ -1,13 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import styled from 'styled-components'
 import Header from './components/header'
-
+import React from 'react';
 import Banner from "./components/Banner";
 import Solutions from "./components/Solutions";
 import Features from "./components/Features";
 import Industries from "./components/Industries";
 import Footer from "./components/Footer";
-import React from 'react';
+
 
 
 import MessengerCustomerChat from 'react-messenger-customer-chat';
@@ -26,6 +26,7 @@ function  Home() {
             <MessengerCustomerChat
                 pageId="331581003847973"
                 appId="613554456622921"
+                themeColor={'#F2F3G2'}
             />,
         </ContainerComp>
 
