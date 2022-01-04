19f28e8d69d0a890edd4aa5fd2eeb889792aa9c0
bilguun.e <bigeservice@gmail.com>
2022-01-04 22:38:10 +0800

    add

--- src/_desktop/Home.js
+++ src/_desktop/Home.js
@@ -25,7 +25,7 @@ function  Home() {
                 pageId="331581003847973"
                 appId="613554456622921"
                 themeColor={'#8753F0'}
-                size="small"
+                size={'small'}
             />,
 
             <Footer/>
