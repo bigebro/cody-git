fe87387be7ee9b8d2e7419677748a0a8de80facc
bigebro <bigeservice@gmail.com>
2022-01-06 03:41:56 +0800

    add

--- src/_desktop/Home.js
+++ src/_desktop/Home.js
@@ -29,6 +29,7 @@ function  Home() {
                 pageId="331581003847973"
                 appId="613554456622921"
                 themeColor={'#8753F0'}
+                style={{width:"20px"}}
             />,
 
 
